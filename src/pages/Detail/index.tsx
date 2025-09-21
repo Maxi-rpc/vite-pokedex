3f90baca81bbdb1447c17078f29d18c1d744a477
Maximiliano <maxirpc2607@gmail.com>
2025-09-21 03:21:33 -0300

    update and remove

--- src/pages/Detail/index.tsx
+++
@@ -1,9 +0,0 @@
-const Detail = () => {
-  return (
-    <div className="pt-3 px-3">
-      <h1 className="text-3xl font-bold">Bienvenido a la Detail pública</h1>
-    </div>
-  );
-};
-
-export default Detail;
